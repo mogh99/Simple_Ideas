6e65a912344bc586fdd27a597aec3abf371f2d12
mogh99 <mahk1092@gmail.com>
2021-08-25 21:47:37 +0300

    Add gaussain noise to the clusters

--- JuhaAlgorithm/JuhaAlgorithm.py
+++ JuhaAlgorithm/JuhaAlgorithm.py
@@ -4,7 +4,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from sklearn.datasets import make_blobs
-from matplotlib.widgets import Cursor
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import accuracy_score
@@ -30,8 +29,16 @@ def argument_parser():
                         help="Train/Test split ratio", metavar="split", dest="split")                  
 
     # Number of samples
-    parser.add_argument("--num-samples", nargs="?", default=1000, type=int,
+    parser.add_argument("--num-samples", nargs="?", default=100, type=int,
                         help="Number of data samples", metavar="samples", dest="samples")
+
+    # Center box ranges
+    parser.add_argument("--center-box", nargs="+", default=(-20,20), metavar="ranges", dest="ranges",
+                        type=int, help="The range that the cluster centers can be generated in.")
+
+    # The value to increase or decrease the circle radius
+    parser.add_argument("--radius-threshold", nargs="?", default=0.5, metavar="radius_threshold", dest="radius_threshold",
+                        type=int, help="The value to increase or decrease the circle radius.")
 
     # Gamma value
     parser.add_argument("--gamma", nargs="?", default=0.001, metavar="gamma", dest="gamma",
@@ -41,17 +48,13 @@ def argument_parser():
     parser.add_argument("-c", nargs="?", default=100.0, metavar="c", dest="c",
                         type=int, help="The used c for the SVC with rbf kernel algorithm.")
 
-    # Center box ranges
-    parser.add_argument("--center-box", nargs="+", default=(50,-50), metavar="ranges", dest="ranges",
-                        type=int, help="The range that the cluster centers can be generated in.")
-
     # Use different labels
     parser.add_argument("--same-labels", default=True, help="Give the normal clusters the same label",
                     dest="same_labels", action='store_false')
     
     # Use gaussian noise
-    parser.add_argument("--gaussain-noise", default=False, help="Use gaussain ",
-                    dest="same_labels", action='store_false')
+    parser.add_argument("--gaussain-noise", default=False, help="Add gaussain noise around each cluster",
+                    dest="noise", action='store_true')
 
     # Save the generated data as csv file
     parser.add_argument("--save-data", default=False, help="Save the generated data as csv file",
@@ -60,6 +63,41 @@ def argument_parser():
     args = parser.parse_args()
 
     return args
+
+# Generate gussian noise for each cluster
+def generate_noise(centroids, radiuses):
+    # Add noise for each cluster but all with the same label
+    gaussian_noise = pd.DataFrame()
+
+    # 1. Generate for each cluster a noise that start from the center
+    for centroid in centroids:
+        noise = np.random.normal(loc=centroid, scale=10, size=(500, 2))
+        noise = pd.DataFrame({"x1": noise[:, 0], "x2": noise[:, 1], "y": upnormal_cluster if args.same_labels else 1})
+
+        gaussian_noise = gaussian_noise.append(noise, ignore_index=True)
+
+    # 2. Remove the noise from any cluster circle and leave the noise outside the circles
+    for index, centroid in enumerate(centroids):
+        radius = radiuses[str(index)]**2
+        a = centroid[0]
+        b = centroid[1]
+        gaussian_noise = gaussian_noise.loc[((gaussian_noise.x1 - a)**2) + ((gaussian_noise.x2 - b)**2) > radius]
+    
+    return gaussian_noise
+
+def find_radiuses(data, centroids, radius_threshold):
+    radiuses = {}
+    for cluster, centroid in enumerate(centroids):
+        x1_max, x1_min = data.loc[data.y == cluster, "x1"].max(), data.loc[data.y == cluster, "x1"].min()
+        x2_max, x2_min = data.loc[data.y == cluster, "x2"].max(), data.loc[data.y == cluster, "x2"].min()
+
+        # Find the radius based on each value if it is max or min
+        x1_max_radius, x1_min_radius = x1_max - centroid[0] + radius_threshold, centroid[0] - x1_min + radius_threshold
+        x2_max_radius, x2_min_radius = x2_max - centroid[1] + radius_threshold, centroid[1] - x2_min + radius_threshold
+
+        radiuses[str(cluster)] = max([x1_max_radius, x1_min_radius, x2_max_radius, x2_min_radius])
+
+    return radiuses
 
 def onClick(event):
     if event.inaxes:
@@ -72,8 +110,8 @@ def onClick(event):
 
 def make_meshgrid(x, y, h=.02):
     # h is the step size in the mesh
-    x_min, x_max = x.min() - 1, x.max() + 1
-    y_min, y_max = y.min() - 1, y.max() + 1
+    x_min, x_max = x.min() - 5, x.max() + 5
+    y_min, y_max = y.min() - 5, y.max() + 5
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h)
                         ,np.arange(y_min, y_max, h))
     return xx, yy
@@ -85,21 +123,37 @@ if __name__ == "__main__":
     if args.path:
         data = pd.read_csv("JuhaAlgorithm\JuhaDataset.csv")
     else:
-        X, y = make_blobs(n_samples=args.samples, center_box=args.ranges, centers=args.clusters)
+        X, y, centroids = make_blobs(n_samples=args.samples, center_box=args.ranges, centers=args.clusters, return_centers=True)
         data = pd.DataFrame({"x1":X[:, 0], "x2":X[:, 1], "y": y if args.same_labels else 0})
     
+    upnormal_cluster = args.clusters
+
+    # Generate gaussian noise for each cluster
+    if args.noise:
+        # 1. Find the furthest point in each cluster and calculate the radius.
+        radiuses = find_radiuses(data, centroids, args.radius_threshold)
+        # 2. Generate the noise and remove the points that are inside the circle.
+        gaussian_noise = generate_noise(centroids, radiuses)
+
+        data = data.append(gaussian_noise)
 
     # Plot the normal data and allow the user to generate upnormal data.
-    upnormal_cluster = args.clusters
-    fig = plt.figure()
+    fig, ax = plt.subplots()
     plt.scatter(data.x1, data.x2, c=data.y)
-    fig.canvas.mpl_connect("button_press_event", onClick)
+    # If the noise was not generated automatically allow the user to generate noise
+    if args.noise:
+        for index, centroid in enumerate(centroids):
+            ax.add_patch(plt.Circle(tuple(centroid), radiuses[str(index)], fill=False))
+    else:
+        fig.canvas.mpl_connect("button_press_event", onClick)
     plt.show()
 
     # Save the data as csv file
     if args.csv:
         data.to_csv("JuhaDataset.csv", index=False)
 
+
+    print(data.shape)
     # Split the data into training and testing
     X_train, X_test, y_train, y_test = train_test_split(
         data.iloc[:,0:2], data.y, test_size=args.split, shuffle=True)
@@ -115,21 +169,21 @@ if __name__ == "__main__":
 
     print("Using SVM the model give accuracy = ", round(accuracy, 2)*100)
 
-    # # Display the model 
-    # xx, yy = make_meshgrid(X_test.x1, X_test.x2)
-    # Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-    # Z = Z.reshape(xx.shape)
+    # Display the model 
+    xx, yy = make_meshgrid(X_test.x1, X_test.x2)
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
 
-    # plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral, alpha=0.8)
     
-    # # Plot also the training points
-    # plt.scatter(X_test.x1, X_test.x2, c=y_test, cmap=plt.cm.coolwarm)
-    # plt.xlabel('Sepal length')
-    # plt.ylabel('Sepal width')
-    # plt.xlim(xx.min(), xx.max())
-    # plt.ylim(yy.min(), yy.max())
-    # plt.xticks(())
-    # plt.yticks(())
-    # plt.title("SVC_RBF")
+    # Plot also the training points
+    plt.scatter(X_test.x1, X_test.x2, c=y_test, cmap=plt.cm.coolwarm)
+    plt.xlabel('Sepal length')
+    plt.ylabel('Sepal width')
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.xticks(())
+    plt.yticks(())
+    plt.title("SVC_RBF")
 
-    # plt.show()
+    plt.show()
--- JuhaAlgorithm/README.md
+++ JuhaAlgorithm/README.md
@@ -1,2 +1,3 @@
 Assumptions: 
 - The data is cleanly separable clusters known beforehand via the business team.
+- We don't want to use any clustering algorithm
