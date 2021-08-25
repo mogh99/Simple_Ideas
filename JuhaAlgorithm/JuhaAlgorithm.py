16c7b89f850afa3d5725b43526545f6aa8358183
mogh99 <mahk1092@gmail.com>
2021-08-25 09:01:08 +0300

    Add all wanted options to the argparse

--- JuhaAlgorithm/JuhaAlgorithm.py
+++ JuhaAlgorithm/JuhaAlgorithm.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 
-global data, upnormal_cluster
+global data, upnormal_cluster, args
 
 def argument_parser():
     parser = argparse.ArgumentParser(prog="Juha Algorithm"
@@ -33,11 +33,27 @@ def argument_parser():
     parser.add_argument("--num-samples", nargs="?", default=1000, type=int,
                         help="Number of data samples", metavar="samples", dest="samples")
 
+    # Gamma value
+    parser.add_argument("--gamma", nargs="?", default=0.001, metavar="gamma", dest="gamma",
+                        type=int, help="The used gamma for the SVC with rbf kernel algorithm.")
+
+    # c value
+    parser.add_argument("-c", nargs="?", default=100.0, metavar="c", dest="c",
+                        type=int, help="The used c for the SVC with rbf kernel algorithm.")
+
     # Center box ranges
     parser.add_argument("--center-box", nargs="+", default=(50,-50), metavar="ranges", dest="ranges",
                         type=int, help="The range that the cluster centers can be generated in.")
 
-    #Save the generated data as csv file
+    # Use different labels
+    parser.add_argument("--same-labels", default=True, help="Give the normal clusters the same label",
+                    dest="same_labels", action='store_false')
+    
+    # Use gaussian noise
+    parser.add_argument("--gaussain-noise", default=False, help="Use gaussain ",
+                    dest="same_labels", action='store_false')
+
+    # Save the generated data as csv file
     parser.add_argument("--save-data", default=False, help="Save the generated data as csv file",
                         dest="csv", action='store_true')
 
@@ -47,7 +63,7 @@ def argument_parser():
 
 def onClick(event):
     if event.inaxes:
-        new_point = [event.xdata, event.ydata, upnormal_cluster]
+        new_point = [event.xdata, event.ydata, upnormal_cluster if args.same_labels else 1]
         data.loc[len(data)] = new_point
     
     plt.scatter(data.x1,data.x2,c=data.y)
@@ -70,7 +86,7 @@ if __name__ == "__main__":
         data = pd.read_csv("JuhaAlgorithm\JuhaDataset.csv")
     else:
         X, y = make_blobs(n_samples=args.samples, center_box=args.ranges, centers=args.clusters)
-        data = pd.DataFrame({"x1":X[:, 0], "x2":X[:, 1], "y":y})
+        data = pd.DataFrame({"x1":X[:, 0], "x2":X[:, 1], "y": y if args.same_labels else 0})
     
 
     # Plot the normal data and allow the user to generate upnormal data.
@@ -89,7 +105,7 @@ if __name__ == "__main__":
         data.iloc[:,0:2], data.y, test_size=args.split, shuffle=True)
 
     # Test the data with a model
-    clf = svm.SVC(gamma=10, C=100.)
+    clf = svm.SVC(gamma=args.gamma, C=args.c)
     clf.fit(X_train, y_train)
 
     predict = clf.predict(X_test)
@@ -99,21 +115,21 @@ if __name__ == "__main__":
 
     print("Using SVM the model give accuracy = ", round(accuracy, 2)*100)
 
-    # Display the model 
-    xx, yy = make_meshgrid(X_test.x1, X_test.x2)
-    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-    Z = Z.reshape(xx.shape)
+    # # Display the model 
+    # xx, yy = make_meshgrid(X_test.x1, X_test.x2)
+    # Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    # Z = Z.reshape(xx.shape)
 
-    plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
+    # plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
     
-    # Plot also the training points
-    plt.scatter(X_test.x1, X_test.x2, c=y_test, cmap=plt.cm.coolwarm)
-    plt.xlabel('Sepal length')
-    plt.ylabel('Sepal width')
-    plt.xlim(xx.min(), xx.max())
-    plt.ylim(yy.min(), yy.max())
-    plt.xticks(())
-    plt.yticks(())
-    plt.title("SVC_RBF")
+    # # Plot also the training points
+    # plt.scatter(X_test.x1, X_test.x2, c=y_test, cmap=plt.cm.coolwarm)
+    # plt.xlabel('Sepal length')
+    # plt.ylabel('Sepal width')
+    # plt.xlim(xx.min(), xx.max())
+    # plt.ylim(yy.min(), yy.max())
+    # plt.xticks(())
+    # plt.yticks(())
+    # plt.title("SVC_RBF")
 
-    plt.show()
+    # plt.show()
