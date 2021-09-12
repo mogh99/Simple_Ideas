4853639a51ee65c6c0de92af827bdc561a62e9cb
mogh99 <mahk1092@gmail.com>
2021-09-12 21:11:01 +0300

    Start generalizing the code to work with matrices instead of 2 dimensions. completed defs generate_noise, generate_data, and find_radiuses.

--- JuhaAlgorithm/JuhaAlgorithm.py
+++ JuhaAlgorithm/JuhaAlgorithm.py
@@ -16,59 +16,67 @@ def argument_parser():
                                                 The algorithm allows the user to generate upnormal data \
                                                 and train a supervised model with the generated dataset.")
 
-    # Number of data clusters
+
+    # Normal Dataset Variables:
+    # 1. Number of data clusters
     parser.add_argument("--clusters", nargs="?", default=2, type=int,
                         help="Number of data clusters", metavar="clusters", dest="clusters")
 
-    #Dataset Path
+    # 2.Dataset Path
     parser.add_argument("--dataset", nargs="?", required=False, type=str, 
                         help="The dataset path", metavar="path", dest="path")
 
-    # Train/Test split ratio
-    parser.add_argument("--split", nargs="?", default=0.33, type=float,
-                        help="Train/Test split ratio", metavar="split", dest="split")                  
-
-    # Number of samples
+    # 3.Number of samples
     parser.add_argument("--num-samples", nargs="?", default=100, type=int,
                         help="Number of data samples", metavar="samples", dest="samples")
 
-    # Center box ranges
+    # 4.Center box ranges
     parser.add_argument("--center-box", nargs="+", default=(-20,20), metavar="ranges", dest="ranges",
                         type=int, help="The range that the cluster centers can be generated in.")
 
-    # The value to increase or decrease the circle radius
-    parser.add_argument("--radius-threshold", nargs="?", default=0.5, metavar="radius_threshold", dest="radius_threshold",
-                        type=float, help="The value to increase or decrease the circle radius.")
+    # 5.Number of features
+    parser.add_argument("--num-features", nargs="?", default=2, type=int,
+                        help="Number of data features (columns, axis, dimensions)", metavar="features", dest="features")
 
-    # The standard deviation for the noise clusters
-    parser.add_argument("--noise-std", nargs="?", metavar="noise_std", dest="noise_std",
-                        type=float, help="The standard deviation for the noise cluster")
-
-    # The number of samples for each noise cluster
-    parser.add_argument("--num-noise", nargs="?", metavar="num_noise", dest="num_noise",
-                        type=int, help="The number of samples for each noise cluster")
-
-    # Gamma value
-    parser.add_argument("--gamma", nargs="?", default=0.001, metavar="gamma", dest="gamma",
-                        type=float, help="The used gamma for the SVC with rbf kernel algorithm.")
-
-    # c value
-    parser.add_argument("-c", nargs="?", default=100.0, metavar="c", dest="c",
-                        type=float, help="The used c for the SVC with rbf kernel algorithm.")
-
-    # Use different labels
-    parser.add_argument("--same-labels", default=True, help="Give the normal clusters the same label",
-                    dest="same_labels", action='store_false')
-    
-    # Use gaussian noise
+    # Noise Dataset Variables: 
+    # 1. Use gaussian noise
     parser.add_argument("--gaussain-noise", default=False, help="Add gaussain noise around each cluster",
                     dest="noise", action='store_true')
 
-    # Save the generated data as csv file
+    # 2. Noise Circle Radius
+    parser.add_argument("--radius-threshold", nargs="?", default=0.5, metavar="radius_threshold", dest="radius_threshold",
+                        type=float, help="The value to increase or decrease the noise circle radius.")
+
+    # 3. Noise Clusters Standard Deviation
+    parser.add_argument("--noise-std", nargs="?", metavar="noise_std", dest="noise_std",
+                        type=float, help="The standard deviation for the noise cluster")
+
+    # 4. Noise Clusters Number of Samples
+    parser.add_argument("--num-noise", nargs="?", metavar="num_noise", dest="num_noise",
+                        type=int, help="The number of samples for each noise cluster")
+
+    # Machine Learning Model Variables:
+    # 1. Gamma value
+    parser.add_argument("--gamma", nargs="?", default=0.001, metavar="gamma", dest="gamma",
+                        type=float, help="The used gamma for the SVC with rbf kernel algorithm.")
+
+    # 2. c value
+    parser.add_argument("-c", nargs="?", default=100.0, metavar="c", dest="c",
+                        type=float, help="The used c for the SVC with rbf kernel algorithm.")
+
+    # 3. Train/Test split ratio
+    parser.add_argument("--split", nargs="?", default=0.33, type=float,
+                        help="Train/Test split ratio", metavar="split", dest="split")                  
+
+    # 4. Use different labels
+    parser.add_argument("--same-labels", default=True, help="Give the normal clusters the same label",
+                    dest="same_labels", action='store_false')
+
+    # 5. Save the generated data as csv file
     parser.add_argument("--save-data", default=False, help="Save the generated data as csv file",
                         dest="csv", action='store_true')
 
-    # Display the generated model
+    # 6. Display the generated model
     parser.add_argument("--display-model", default=False, help="Display the generated model",
                         dest="display_model", action='store_true')
 
@@ -79,38 +87,64 @@ def argument_parser():
 
     return args
 
+# Generate the normal clusters using make_blobs
+def generate_data(X, y):
+    y = y.reshape(len(y), 1)
+    data = np.hstack((X, y))
+
+    columns = [f"x{i}" for i in range(0, X.shape[1])]
+    columns.append("y")
+
+    df = pd.DataFrame(data, columns=columns)
+
+    return df
+
 # Generate gussian noise for each cluster
 def generate_noise(centroids, radiuses):
     # Add noise for each cluster but all with the same label
     gaussian_noise = pd.DataFrame()
+    columns = [f"x{i}" for i in range(0, args.features)]
+    columns.append("y")
 
     # 1. Generate for each cluster a noise that start from the center
     for centroid in centroids:
-        noise = np.random.normal(loc=centroid, scale=args.noise_std, size=(args.num_noise, 2))
-        noise = pd.DataFrame({"x1": noise[:, 0], "x2": noise[:, 1], "y": upnormal_cluster if args.same_labels else 1})
+        noise = np.random.normal(loc=centroid, scale=args.noise_std, size=(args.num_noise, args.features))
+        
+        y = np.array([upnormal_cluster if args.same_labels else 1 for i in range(0, noise.shape[0])])
+        y = y.reshape(noise.shape[0], 1)
+        noise = np.hstack((noise, y))
+
+        noise = pd.DataFrame(noise, columns=columns)
 
         gaussian_noise = gaussian_noise.append(noise, ignore_index=True)
 
-    # 2. Remove the noise from any cluster circle and leave the noise outside the circles
-    for index, centroid in enumerate(centroids):
-        radius = radiuses[str(index)]**2
-        a = centroid[0]
-        b = centroid[1]
-        gaussian_noise = gaussian_noise.loc[((gaussian_noise.x1 - a)**2) + ((gaussian_noise.x2 - b)**2) > radius]
+    # 2. Remove the noise from any cluster n-sphere and leave the noise outside the n-sphere
+    # radius ^ 2 = (x1 - c1)^2 + (x2 - c2)^2 + ... + (xn - cn)^2
+    # n-sphere is a sphere with n-dimensions
+    for cluster, centroid in enumerate(centroids):
+        value = 0
+        radius = radiuses[cluster]**2
+
+        for index, feature in enumerate(data.columns[:-1]):
+            value += (gaussian_noise[feature] - centroid[index])**2
+        
+        gaussian_noise = gaussian_noise.loc[(value > radius)]
     
     return gaussian_noise
 
 def find_radiuses(data, centroids, radius_threshold):
     radiuses = {}
+
+    # For each cluster find the furthest distance from the centroid for all the features.
     for cluster, centroid in enumerate(centroids):
-        x1_max, x1_min = data.loc[data.y == cluster, "x1"].max(), data.loc[data.y == cluster, "x1"].min()
-        x2_max, x2_min = data.loc[data.y == cluster, "x2"].max(), data.loc[data.y == cluster, "x2"].min()
+        furthests = []
+        for index, feature in enumerate(data.columns[:-1]):
+            feature_max, feature_min = data.loc[data.y == cluster, feature].max(), data.loc[data.y == cluster, feature].min()
+            feature_max_radius, feature_min_radius = feature_max - centroid[index] + radius_threshold, centroid[index] - feature_min + radius_threshold
 
-        # Find the radius based on each value if it is max or min
-        x1_max_radius, x1_min_radius = x1_max - centroid[0] + radius_threshold, centroid[0] - x1_min + radius_threshold
-        x2_max_radius, x2_min_radius = x2_max - centroid[1] + radius_threshold, centroid[1] - x2_min + radius_threshold
-
-        radiuses[str(cluster)] = max([x1_max_radius, x1_min_radius, x2_max_radius, x2_min_radius])
+            furthests += [feature_max_radius, feature_min_radius]
+        
+        radiuses[cluster] = max(furthests)
 
     return radiuses
 
@@ -135,11 +169,12 @@ if __name__ == "__main__":
     args = argument_parser()
 
     # Generate data as clusters and give them the same label.
+    # TODO: If the data was read through a file we need to make sure the dataset has y feature and the centroid are known this issue can be solved using k-means algorithm 
     if args.path:
         data = pd.read_csv("JuhaAlgorithm\JuhaDataset.csv")
     else:
-        X, y, centroids = make_blobs(n_samples=args.samples, center_box=args.ranges, centers=args.clusters, return_centers=True)
-        data = pd.DataFrame({"x1":X[:, 0], "x2":X[:, 1], "y": y if args.same_labels else 0})
+        X, y, centroids = make_blobs(n_features=args.features, n_samples=args.samples, center_box=args.ranges, centers=args.clusters, return_centers=True)
+        data = generate_data(X, y)
     
     upnormal_cluster = args.clusters
 
@@ -152,16 +187,17 @@ if __name__ == "__main__":
 
         data = data.append(gaussian_noise)
 
-    # Plot the normal data and allow the user to generate upnormal data.
-    fig, ax = plt.subplots()
-    plt.scatter(data.x1, data.x2, c=data.y)
-    # If the noise was not generated automatically allow the user to generate noise
-    if args.noise:
-        for index, centroid in enumerate(centroids):
-            ax.add_patch(plt.Circle(tuple(centroid), radiuses[str(index)], fill=False))
-    else:
-        fig.canvas.mpl_connect("button_press_event", onClick)
-    plt.show()
+    # Plot the normal data and allow the user to generate upnormal data only if the number of features equal 2.
+    if args.features == 2:
+        fig, ax = plt.subplots()
+        plt.scatter(data.x0, data.x1, c=data.y)
+        # If the noise was not generated automatically allow the user to generate noise
+        if args.noise:
+            for index, centroid in enumerate(centroids):
+                ax.add_patch(plt.Circle(tuple(centroid), radiuses[index], fill=False))
+        else:
+            fig.canvas.mpl_connect("button_press_event", onClick)
+        plt.show()
 
     # Save the data as csv file
     if args.csv:
@@ -169,7 +205,7 @@ if __name__ == "__main__":
 
     # Split the data into training and testing
     X_train, X_test, y_train, y_test = train_test_split(
-        data.iloc[:,0:2], data.y, test_size=args.split, shuffle=True)
+        data.iloc[:,0:args.features], data.y, test_size=args.split, shuffle=True)
 
     # Test the data with a model
     clf = svm.SVC(gamma=args.gamma, C=args.c)
@@ -182,16 +218,17 @@ if __name__ == "__main__":
 
     print("Using SVM the model give accuracy = ", round(accuracy, 2)*100)
 
-    if args.display_model:
+    # Display the trained model if only the number of features equals 2
+    if args.display_model and args.features == 2:
         # Display the model 
-        xx, yy = make_meshgrid(X_test.x1, X_test.x2)
+        xx, yy = make_meshgrid(X_test.x0, X_test.x1)
         Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
         Z = Z.reshape(xx.shape)
 
         plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral, alpha=0.8)
         
         # Plot also the training points
-        plt.scatter(X_test.x1, X_test.x2, c=y_test, cmap=plt.cm.coolwarm)
+        plt.scatter(X_test.x0, X_test.x1, c=y_test, cmap=plt.cm.coolwarm)
         plt.xlabel('Sepal length')
         plt.ylabel('Sepal width')
         plt.xlim(xx.min(), xx.max())
